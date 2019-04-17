0120cb4f2bdecb065f95ae0f773fdd880a649253
=Raunak <=sharmaraunak338@gmail.com>
2019-04-17 16:55:36 +0530

    datatypes explained

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+//number,can be positive ,negative or decimal
+let x = 7;
+console.log(typeof x); //type of variable assigned to x.
+//Boolean
+let y = true; //can be true or false
+console.log(typeof y);
+//String
+let z = "Raunak Sharma"; //can be any seq of chars
+console.log(typeof z);
+
+let a;
+console.log(a);
+console.log(typeof a);
