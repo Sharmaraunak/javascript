acceac8883f4351bd9fce3abf51f493260c93a33
=Raunak <=sharmaraunak338@gmail.com>
2019-04-17 16:47:53 +0530

    variables keywords

---
+++ variables.js
@@ -0,0 +1,26 @@
+//variables
+let x = 7; //let is a keyword
+let y = 8; //technique is called initialization
+let z = x + y; //expression
+console.log("Answer: " + z);
+//ways to declare variables
+var a = 7; //var is a keyword
+console.log(a); //It works as same as let
+//variable to never change the value
+const f = 7; //can't change the value
+f = 6; //throws error
+//assign value as many as you want but declare them only once
+let z;
+console.log(z); //return undefined because it has not be defined
+//identifier rule
+//1.start with a letter or _
+//2.don't start with a number
+//3.don't use keyword like let,var ,console etc.
+//4.case sensitive x and X are different identifier
+/*code conventions
+1.use descriptive identifiers
+let firstNumber   = 3;
+2.camelCasing:
+1st syllable should start with lowercase other Capital.
+3.stay consistent/
+*/
