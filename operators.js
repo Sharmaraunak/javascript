b50a6e6ed7c1d539cc5d5e82f57686c0f6c47e89
=Raunak <=sharmaraunak338@gmail.com>
2019-04-17 17:23:59 +0530

    types of operators

---
+++ operators.js
@@ -0,0 +1,30 @@
+//catogories of operators
+//assignment =
+//Arithmatic + - * / %
+
+var a = 10 % 3; //gives remainder
+console.log("remainder:" + a);
+
+//Increment/Decrement ++ --
+var a = 1;
+a++;
+console.log(a); //a = 2
+console.log(a++); //first logging then incrementation.
+console.log(a);
+console.log(++a); //first addition then logging  will happen
+
+// String  '' +(concotation)
+//Precedence
+var b = 1 + 2 * 3; //follows the rule of algebra
+console.log(b);
+
+//function invokation operator ()
+console.log("hello");
+
+//Logical and: &&  or: ||
+
+//Member accessor operator
+//the dot(period) comes under this.
+//code block operators { }
+
+//Array element accessor [ ]
