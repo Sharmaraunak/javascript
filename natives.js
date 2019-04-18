f92d83c47de946c16745b25c8178cb18272cf492
=Raunak <=sharmaraunak338@gmail.com>
2019-04-18 19:34:36 +0530

    primitives

---
+++ natives.js
@@ -0,0 +1,29 @@
+//string inbuilt functions
+let myString = new String("hello");
+
+console.log(myString);
+console.log(myString.toString()); //conver object to string
+console.log(typeof myString); //object
+
+/**primitive functions
+ * String()
+ * Number()
+ * Boolean()
+ * Object()
+ * Function()
+ * Symbol()
+ */
+/**
+ * Array()
+ * RegExp()
+ */
+let myPrimitive = "MY NAME IS RAUNAK KUMAR SHARMA";
+console.log(typeof myPrimitive);
+myPrimitive = myPrimitive.toLowerCase();
+console.log(myPrimitive);
+console.log(typeof myPrimitive);
+
+let myNumber = new Number(7);
+console.log(typeof myNumber);
+let myPrimitive2 = myNumber.valueOf();
+console.log(typeof myPrimitive2);
