f88bd04a467e663046c0ca500cff1b08f10f41f6
=Raunak <=sharmaraunak338@gmail.com>
2019-04-18 11:42:46 +0530

    if-else if -else conditions

---
+++ decision.js
@@ -0,0 +1,11 @@
+//If-else statement
+count = 0;
+if (/*condition*/ count == 1) {
+  console.log("count is 1");
+} else if (count > 1) {
+  console.log("count is greate then 1");
+} else {
+  console.log("count is  not 1");
+}
+
+//
