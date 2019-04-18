11d234ff943d58689f140fc41e847da00d7cb6e1
=Raunak <=sharmaraunak338@gmail.com>
2019-04-18 18:59:45 +0530

    template literal ${}

---
+++ template-literal.js
@@ -0,0 +1,27 @@
+let name = "bob";
+//injecting a variable from outside in log.
+console.log(`hi ${name}`);
+
+/* let sentence = `a set of words 
+    that is complete in itself,
+    typically containing a 
+    subject and predicate, 
+    conveying a statement, 
+    question, exclamation,
+    or command, 
+    and consisting of a 
+    main clause and sometimes 
+    one or more subordinate 
+    clauses.`;
+
+console.log(sentence); */
+
+function getReasonCount() {
+  return 2;
+}
+
+let interpolation = `Give me ${
+  getReasonCount() == 1 ? "one good reason" : "a few reasons"
+} to try this`;
+
+console.log(interpolation);
