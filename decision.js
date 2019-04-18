92765bf6ae7e67d3a567e3b3191a8a9c867e1483
=Raunak <=sharmaraunak338@gmail.com>
2019-04-18 11:54:40 +0530

    ternary operators

--- decision.js
+++ decision.js
@@ -21,3 +21,12 @@ switch (hero) {
     console.log("member of JLA");
     break;
 }
+
+//ternary operator
+let a = 1,
+  b = "1";
+let result = a == b ? "equal" : "inequal";
+console.log(result);
+
+//strict equality checks the value and  the type
+console.log(1 === "1");
