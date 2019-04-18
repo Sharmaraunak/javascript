0242520fa45ec6bf2484813824bf7920cbf2a9ed
=Raunak <=sharmaraunak338@gmail.com>
2019-04-18 18:49:55 +0530

    mapping one data structure to another - destructuring

---
+++ destructuring.js
@@ -0,0 +1,28 @@
+let a, b, c, d, e;
+
+let names = ["david", "eddie", "alex", "michael", "sammy"];
+[a, b, c, e, d] = names;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+//destructuring
+let others;
+[a, b, ...others] = names;
+console.log(a);
+console.log(b);
+// will assign one-onr to a and b ,rest to others as array
+console.log(others);
+
+//case 3:another way to destruct
+let year, model;
+({ year, model } = {
+  make: "bmw",
+  model: "745li",
+  year: 2010,
+  value: 5000
+});
+console.log(year);
+console.log(model);
