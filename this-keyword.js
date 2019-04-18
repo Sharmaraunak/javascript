d4825ebc305bb30463f553869ba69c0ec40f6312
=Raunak <=sharmaraunak338@gmail.com>
2019-04-18 18:06:20 +0530

    this keyword

---
+++ this-keyword.js
@@ -0,0 +1,46 @@
+/* let car = {
+  make: "bmw",
+  model: "745li",
+  year: 2004,
+  getPrice: function() {
+    //perform some calc
+    return 5000;
+  },
+  printDescription: function() {
+    console.log(this.make + " : " + this.model);
+  }
+}; */
+
+function first() {
+  return this;
+}
+
+console.log(first() === global);
+/**
+ * this: it is Node's global object
+ * because that's where the first method was called.
+ */
+
+function second() {
+  "use strict"; //gets on bound to nothing
+
+  return this;
+}
+
+console.log(second() === global);
+console.log(second() === undefined);
+
+let myObject = { value: "myObject" };
+//value is set on global object
+global.value = "global object";
+
+function third(name) {
+  return this.value + name;
+}
+
+console.log(third());
+//bind with call inbuilt fun
+console.log(third.call(myObject, "bob"));
+
+//with apply ,array is used
+console.log(third.apply(myObject, ["bob"]));
