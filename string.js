fb47913e6ff04cf34c2d1163b16afaf473c3e24a
=Raunak <=sharmaraunak338@gmail.com>
2019-04-19 10:45:40 +0530

    strings

---
+++ string.js
@@ -0,0 +1,21 @@
+let first =
+  "When you’re playing small and want to break out of your current limitations…";
+
+let second =
+  "When you want to make your life feel better and your thoughts more positive";
+
+let third = "45,56,23,58,96,45";
+
+//you can call string methods on string literals
+console.log("Raunak loves you".toUpperCase());
+
+/**
+ * split:will split you string into pieces according to character
+ * given
+ * result will be an array
+ */
+let mySplit = third.split(",");
+console.log(mySplit);
+
+let mySubdtr = first.substring(13, 5);
+console.log(mySubdtr);
